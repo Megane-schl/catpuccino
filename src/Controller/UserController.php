569cc8c1391113b7f5f75cbc2a3ac772ae03f7e1
Mégane <megane.schaetzel@gmail.com>
2026-04-13 01:56:37 +0200

    feat: is ban méthode, delete soft + accès admin

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class UserController extends AbstractController
 {
@@ -21,6 +22,7 @@ final class UserController extends AbstractController
      * @return Response the list of the users
      */
     #[Route('/user', name: 'app_user')]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(UserRepository $userRepository): Response
     {
         $arrUsers = $userRepository->findAll();
@@ -38,6 +40,7 @@ final class UserController extends AbstractController
      */
 
     #[Route('/user/create', name: 'app_user_create')]
+    #[IsGranted('ROLE_ADMIN')]
     public function create(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         $objUser = new User();
@@ -86,7 +89,8 @@ final class UserController extends AbstractController
      * @param EntityManagerInterface Use to save and change data
      * @return Response The success or the failure of updating the user and redirect to the user list
      */
-    #[Route('/user/{id<\d+>}', name: 'app_user_update')]
+    #[Route('/user/{id<\d+>}/update', name: 'app_user_update')]
+    #[IsGranted('ROLE_ADMIN')]
     public function update(
         User $user,
         Request $request,
@@ -109,9 +113,11 @@ final class UserController extends AbstractController
                 $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
             }
 
+            $user->setUpdatedAt(new DateTimeImmutable('now'));
+
             $entityManager->flush();
 
-            $this->addFlash('success', "L'utilisateur a été modifié");
+            $this->addFlash('success', "L'utilisateur a été mis à jour");
 
             return $this->redirectToRoute('app_user');
         }
@@ -131,6 +137,7 @@ final class UserController extends AbstractController
      * @return Response The success or the failure of updating the user and redirect to the user list
      */
     #[Route('/user/{id<\d+>}/roles', name: 'app_user_roles')]
+    #[IsGranted('ROLE_ADMIN')]
     public function updateRoles(
         User $user,
         Request $request,
@@ -170,5 +177,58 @@ final class UserController extends AbstractController
             'title'     => 'Affecter des rôles',
             'subtitle'  => 'Modifier les rôles de ' . $user->getFirstname() . ' ' . $user->getLastname()
         ]);
+    }
+
+    /**
+     * Method to soft delete an user
+     * @param User $user The user to delete
+     * @param EntityManagerInterface $entityManager Use to save and change the data
+     * @return Response The success or the failure of deleting the user and redirect to the user list
+     */
+    #[Route('/user/{id<\d+>}/delete', name: 'app_user_delete')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function delete(User $user, EntityManagerInterface $entityManager): Response
+    {
+        //in case the admin try to delete himself
+        if ($user == $this->getUser()) {
+
+            $this->addFlash('danger', "Vous ne pouvez pas supprimer votre propre compte");
+            return $this->redirectToRoute('app_user');
+        }
+
+        $user->setDeletedAt(new DateTimeImmutable('now'));
+        $entityManager->flush();
+        $this->addFlash('success', "L'utilisateur a été supprimé");
+
+        return $this->redirectToRoute('app_user');
+    }
+
+    /**
+     * Method to ban or urban an user
+     * @param User $user The user ban/unban
+     * @param EntityManagerInterface $entityManager Use to save and change the data
+     * @return Response The success or the failure of banning/unbanning the user and redirect to the user list
+     */
+    #[Route('/user/{id<\d+>}/ban', name: 'app_user_ban')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function ban(User $user, EntityManagerInterface $entityManager): Response
+    {
+        //in case the admin try to ban himself
+        if ($user == $this->getUser()) {
+
+            $this->addFlash('danger', "Vous ne pouvez pas bannir votre propre compte");
+            return $this->redirectToRoute('app_user');
+        }
+
+        if ($user->isBan()) {
+            $user->setIsBan(false);
+            $this->addFlash('warning', "L'utilisateur a été débanni");
+        } else {
+            $user->setIsBan(true);
+            $this->addFlash('success', "L'utilisateur a été banni");
+        }
+
+        $entityManager->flush();
+        return $this->redirectToRoute('app_user');
     }
 }
